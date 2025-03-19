64f525f53dee2fabf5c1d17b5eff385503269fca
Fernando Talavera <ftalaveram01@gmail.com>
2025-03-19 20:07:24 +0100

    creacion de componente inicio y primer diseño de pagina para ordenador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { InicioComponent } from './Components/inicio/inicio.component';
+import { HomeComponent } from './Components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent},
-    { path: 'login', component: LoginComponent},
-    { path: 'register', component: RegisterComponent},
-    { path: 'inicio', component: InicioComponent}
+    { path: '', component: InicioComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'home', component: HomeComponent }
 ];
