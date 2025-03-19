745f102e22a10ff6ba34fec5b4db64bf0efb15d7
Fernando Talavera <ftalaveram01@gmail.com>
2025-03-19 20:30:31 +0100

    Deteccion de movil

--- src/app/Components/inicio/inicio.component.ts
+++ src/app/Components/inicio/inicio.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
 
-  constructor(private router: Router) { }
+  isMobile: boolean = false;
+
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([
+      Breakpoints.HandsetPortrait,
+      Breakpoints.TabletPortrait
+    ]).subscribe(result => {
+      this.isMobile = result.matches;
+    }
+    );
+  }
 
   ngOnInit() { }
 
