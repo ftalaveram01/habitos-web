a743096fc4803e58c1de3d850eed8c9b7b868344
Fernando Talavera <ftalaveram01@gmail.com>
2025-03-20 19:33:52 +0100

    Estilos de inicio con media para deteccion de dispositivo

--- src/app/Components/inicio/inicio.component.ts
+++ src/app/Components/inicio/inicio.component.ts
@@ -1,45 +1,18 @@
 import { Component, HostBinding } from '@angular/core';
 import { Router } from '@angular/router';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { trigger, transition, animate, style, state } from '@angular/animations';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css',
-  animations: [
-    trigger('fadeIn', [
-      state('void', style({ opacity: 0, visibility: 'hidden' })),
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('500ms 100ms', style({ opacity: 1, visibility: 'visible' }))
-      ]),
-      transition(':leave', [
-        animate('500ms', style({ opacity: 0 }))
-      ])
-    ])
-  ]
+  styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
 
-  isMobile: boolean = false;
-  isReady: boolean = false;
+  constructor(private router: Router) {
 
-  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {
-    this.breakpointObserver.observe([
-      Breakpoints.HandsetPortrait,
-      Breakpoints.TabletPortrait
-    ]).subscribe(result => {
-      this.isMobile = result.matches;
-    }
-    );
-  }
-
-  ngOnInit() {
-    this.isReady = true;
   }
 
   irALogin() {
