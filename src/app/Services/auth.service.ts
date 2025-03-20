a2fca5c926bd0268e624dffcf0a6d45ef3357633
Fernando <ftalaveram01@gmail.com>
2025-03-20 09:16:39 +0100

    cambio de endpoint para auth

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -5,31 +5,31 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthService {
-  
-  apiHabitosAuthUrl = 'http://localhost:8080/rest/usuarios';
+
+  apiHabitosAuthUrl = 'http://localhost:8080/usuarios';
 
   constructor(private http: HttpClient) { }
 
-  Login(username: string, password: string, onLogin: (ok: boolean, user?:any) => void){
-    
+  Login(username: string, password: string, onLogin: (ok: boolean, user?: any) => void) {
+
     const params = new HttpParams()
       .set('email', username)
       .set('password', password);
 
-    this.http.get(`${this.apiHabitosAuthUrl}/login`, {params}).subscribe((users :any) => {
-        if(users){
-          onLogin(true,users)
-        }
-      },
+    this.http.get(`${this.apiHabitosAuthUrl}/login`, { params }).subscribe((users: any) => {
+      if (users) {
+        onLogin(true, users)
+      }
+    },
       (error) => {
-        if(error.status === 404){
+        if (error.status === 404) {
           onLogin(false, undefined)
         }
       }
     );
   }
 
-  Register(user: any, onRegister: (ok: boolean) => void){
+  Register(user: any, onRegister: (ok: boolean) => void) {
 
     const usuario = {
       nombre: user.name,
@@ -37,13 +37,13 @@ export class AuthService {
       password: user.password
     }
 
-    this.http.post(`${this.apiHabitosAuthUrl}/register`, usuario).subscribe((users :any) => {
-        if(users){
-          onRegister(true)
-        }
-      },
+    this.http.post(`${this.apiHabitosAuthUrl}/register`, usuario).subscribe((users: any) => {
+      if (users) {
+        onRegister(true)
+      }
+    },
       (error) => {
-        if(error.status === 400){
+        if (error.status === 400) {
           onRegister(false)
         }
       }
