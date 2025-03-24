8a655c800d4fdf944840b0c052859e0b7bdecaaf
Fernando Talavera <ftalaveram01@gmail.com>
2025-03-24 19:39:38 +0100

    Nuevo Componente para mostrar habitos publicos

--- src/app/Components/recomendados/recomendados.component.ts
+++ src/app/Components/recomendados/recomendados.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { HabitosService } from '../../Services/habitos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recomendados',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recomendados.component.html',
   styleUrl: './recomendados.component.css'
 })
 export class RecomendadosComponent {
+
+  habitos: any = [];
+
+  constructor(private habitosService: HabitosService) { }
+
+  ngOnInit(): void {
+    this.habitosService.getHabitosRecomendados().subscribe((habitos: any) => {
+      this.habitos = habitos;
+    });
+  }
 
 }
