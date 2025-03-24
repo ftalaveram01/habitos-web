2d59861a9d1a793483a626c6051c987c0a349b23
Fernando Talavera <ftalaveram01@gmail.com>
2025-03-24 20:33:35 +0100

    cargar los habitos sin estilos dados

--- src/app/Components/mishabitos/mishabitos.component.ts
+++ src/app/Components/mishabitos/mishabitos.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { HabitosService } from '../../Services/habitos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mishabitos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mishabitos.component.html',
   styleUrl: './mishabitos.component.css'
 })
 export class MishabitosComponent {
 
+  habitos: any = [];
+
+  constructor(private habitosService: HabitosService) { }
+
+  ngOnInit(): void {
+    this.habitosService.getHabitos().subscribe((habitos: any) => {
+      this.habitos = habitos;
+    });
+  }
 }
