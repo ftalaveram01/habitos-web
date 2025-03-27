0ffdc4d57bad96795c65a510bc680a1849af23fb
Fernando <ftalaveram01@gmail.com>
2025-03-27 16:44:27 +0100

    carga de datos en el componente

--- src/app/Components/perfil/perfil.component.ts
+++ src/app/Components/perfil/perfil.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../Services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
 })
 export class PerfilComponent {
 
-  habitos: any = []
+  nombre: String = '';
+  email: String = '';
+  numeroHabitos: Number = 0
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
-    console.log(this.userService.getUserData().subscribe((habitos: any) => {
-      this.habitos = habitos;
-    }))
+    this.userService.getUserData().subscribe((data: any) => {
+      this.nombre = data.nombre;
+      this.email = data.email;
+      this.numeroHabitos = data.numeroHabitos;
+    })
   }
 }
