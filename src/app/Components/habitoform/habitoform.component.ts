4af75d812203c9b8485a9da13d5b949c559ea30d
Fernando Talavera <ftalaveram01@gmail.com>
2025-03-30 17:18:04 +0200

    nuevo form de nuevo habito

--- src/app/Components/habitoform/habitoform.component.ts
+++ src/app/Components/habitoform/habitoform.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-habitoform',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './habitoform.component.html',
   styleUrl: './habitoform.component.css'
 })
 export class HabitoformComponent {
 
+  form !: FormGroup;
+  errors: { [nameError: string]: string } = {};
+
+  habito: any = {
+    nombre: '',
+    descripcion: '',
+    frecuencia: '',
+    duracion: ''
+  };
+
+
+  constructor(private router: Router, private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      nombre: [''],
+      descripcion: [''],
+      frecuencia: [''],
+      duracion: ['']
+    });
+  }
+
+  onSubmit() {
+    // Handle form submission
+  }
 }
