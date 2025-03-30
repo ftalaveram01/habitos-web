23cbe2dddc6ace29beb3f2c9e5bf7b70265bef10
Fernando Talavera <ftalaveram01@gmail.com>
2025-03-30 16:35:12 +0200

    ruta nueva para nuevo habito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { InicioComponent } from './Components/inicio/inicio.component';
 import { HomeComponent } from './Components/home/home.component';
+import { HabitoformComponent } from './Components/habitoform/habitoform.component';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'home/new', component: HabitoformComponent },
 ];
